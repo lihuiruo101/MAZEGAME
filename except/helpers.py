8d625b0b8fe0f9b9f61400df8c75e1125a84bb24
lihuiruo101 <yjy140206@gmail.com>
2025-05-08 22:37:11 +0900

    Create helpers.py

---
+++ except/helpers.py
@@ -0,0 +1,8 @@
+def show_help():
+    print("""
+    Help - 게임 설명
+    -------------------
+    - ESC: Get Code (이어하기 코드와 인증번호 생성)
+    - Ctrl: Save Code (저장된 코드를 통해 게임을 이어서 할 수 있음)
+    - Alt: Help (게임의 기능에 대해 설명)
+    """)
